f7dfa8d21be5a93a3946bc691baf0e54ffb700f0
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-05-07 14:15:32 -0400

    chore: add arrayPluseArray method

--- challenges/Main.java
+++ challenges/Main.java
@@ -1,7 +1,14 @@
+import java.util.stream.*;
+
+
 class Main { 
   public static void main(String[] args) {
-    System.out.println(addLetters("a", "b"));
-    System.out.println(addLetters("a"));
+    // System.out.println(addLetters("a", "b"));
+    // System.out.println(addLetters("a"));
+
+    int[] arr1 =  { 1,2,3 };
+    int[] arr2 = { 4,5,6 };
+    System.out.println(arrayPluseArray(arr1, arr2 ));
   }
 
   public static String addLetters(String... letters) {
@@ -29,5 +36,10 @@ class Main {
 
     // convert the target char
     return Character.toString(alphabet.charAt(sum - 1));
+  }
+
+  public static int arrayPluseArray(int[] arr1, int[]  arr2) {
+    return IntStream.of(arr1).sum() + IntStream.of(arr2).sum();
+
   }
 }

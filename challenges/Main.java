0026c7c13b36e974046fb209c31fdac1d66d2538
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-05-07 16:27:30 -0400

    chore: add points

--- challenges/Main.java
+++ challenges/Main.java
@@ -1,14 +1,17 @@
 import java.util.stream.*;
-
+import java.util.*;
 
 class Main { 
   public static void main(String[] args) {
     // System.out.println(addLetters("a", "b"));
     // System.out.println(addLetters("a"));
 
-    int[] arr1 =  { 1,2,3 };
-    int[] arr2 = { 4,5,6 };
-    System.out.println(arrayPluseArray(arr1, arr2 ));
+    // int[] arr1 =  { 1,2,3 };
+    // int[] arr2 = { 4,5,6 };
+    // System.out.println(arrayPluseArray(arr1, arr2 ));
+
+    System.out.println(points(new String[]
+    {"1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"}));
   }
 
   public static String addLetters(String... letters) {
@@ -41,5 +44,23 @@ class Main {
   public static int arrayPluseArray(int[] arr1, int[]  arr2) {
     return IntStream.of(arr1).sum() + IntStream.of(arr2).sum();
 
+  }
+
+  public static int points(String[] games) {
+    int points = 0; 
+
+    for(String game : games) {
+      // make arry of strings split 
+      String[] scores = game.split(":");
+      
+      System.out.println("This is scores: " + Arrays.toString(scores));
+      int x = Integer.parseInt(scores[0]);
+      int y = Integer.parseInt(scores[1]);
+      
+
+      points += x > y ? 3 : x < y ? 1 : 0; 
+    }
+
+    return points; 
   }
 }

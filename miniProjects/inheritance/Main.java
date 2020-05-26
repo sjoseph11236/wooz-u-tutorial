00649e978fcc1665bed0dbc9169956f9bd7ebe2a
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-05-25 20:41:53 -0400

    feat: created Vehicle class

---
+++ miniProjects/inheritance/Main.java
@@ -0,0 +1,9 @@
+import inventory.Vehicle; 
+
+class Main {
+  public static void main(String[] args) {
+    
+    Vehicle myVechile = new Vehicle();
+
+  }
+}
---
+++ miniProjects/inheritance/inventory/Vehicle.java
@@ -0,0 +1,31 @@
+package inventory;
+
+public class Vehicle {
+
+  private String make;
+  private String model; 
+
+  public Vehicle() {}
+
+  public Vehicle(String make, String model) {
+    this.make = make;
+    this.model = model; 
+  }
+
+
+  public String getMake() {
+    return make;
+  }
+
+  public String getModel() {
+    return model;
+  }
+
+  public void setMake(String make) {
+    this.make = make;
+  }
+
+  public void setModel(String model) {
+    this.model = model;
+  }
+}
